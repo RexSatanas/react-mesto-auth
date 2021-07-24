bfc2acd858f382e67b76b48901dc03ecf15724cc
Denis Myagotin <rexsatanas@yandex.ru>
2021-07-25 00:53:33 +0300

    version 12.3 fixing login page

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/mesto-react-auth">
         <App />
     </BrowserRouter>
   </React.StrictMode>,
