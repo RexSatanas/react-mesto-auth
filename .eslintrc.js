e3cba7f7ec95c70742679a516b8357294f203825
Denis Myagotin <rexsatanas@yandex.ru>
2021-07-25 14:26:03 +0300

    start

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    "airbnb-base"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "no-underscore-dangle": [2, {
+      "allow": ["_id"]
+    }]
+  },
+};
